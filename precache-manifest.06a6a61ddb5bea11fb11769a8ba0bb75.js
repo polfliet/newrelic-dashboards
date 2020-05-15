26160019e47db542353001afb29a5e82b1e7f403
kidk <samuel@sava.be>
2020-05-15 17:03:15 +0000

    Deploying to gh-pages from  @ 98ee7b808c8964a9c07a1216208f5d6e282a36b1 🚀

---
+++ precache-manifest.06a6a61ddb5bea11fb11769a8ba0bb75.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f10811e493027d1f45bc05e3249b9c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bce84fe1efeb4110da13",
+    "url": "/static/js/2.669fb115.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.669fb115.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf110a2af884823a77c4",
+    "url": "/static/js/main.e5df3bcf.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  }
+]);
