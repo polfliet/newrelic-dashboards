5e4d94477009f22fc25244a7d88bca8254e99495
kidk <samuel@sava.be>
2020-05-18 08:55:59 +0000

    Deploying to gh-pages from  @ 917a9684340deecfdfa5ea747adab2566f51e646 🚀

---
+++ precache-manifest.2eefef064bc12dc5ee87a99cc5ba9d03.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "675d88eb166d94cefc23727040fa02f6",
+    "url": "/newrelic-dashboards/index.html"
+  },
+  {
+    "revision": "0f525cf1a8928389174f",
+    "url": "/newrelic-dashboards/static/js/2.77610a99.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/newrelic-dashboards/static/js/2.77610a99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c961eef7ec4a24fce003",
+    "url": "/newrelic-dashboards/static/js/main.ba6bf707.chunk.js"
+  },
+  {
+    "revision": "d233718d3c81fbb66986",
+    "url": "/newrelic-dashboards/static/js/runtime-main.57942c54.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/newrelic-dashboards/precache-manifest.f3e47bd15bafbce1fb8172c316bc45f5.js"
+  "/newrelic-dashboards/precache-manifest.2eefef064bc12dc5ee87a99cc5ba9d03.js"
 );
 
 self.addEventListener('message', (event) => {
